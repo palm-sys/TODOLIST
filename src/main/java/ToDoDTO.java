3ada453eaf399dd4491ac45f1e8d4dd8030955fa
palm <2788207917@qq.com>
2026-01-02 17:32:13 +0800

    todo的DTO类

---
+++ src/main/java/ToDoDTO.java
@@ -0,0 +1,15 @@
+
+public class ToDoDTO {
+
+    //代办类型
+    private String todoClss;
+
+
+    public String getTodoClss() {
+        return todoClss;
+    }
+
+    public void setTodoClss(String todoClss) {
+        this.todoClss = todoClss;
+    }
+}
